6d15bdfb2cc6884fe96f0394341c2ea78246d9ad
Jacob Modayil <jmodayil@deloitte.com>
2020-03-04 20:28:52 -0600

    Added Course Service and Course Repository

---
+++ CleanArchitecture/CleanArchitecture.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Application.ViewModels;
+using CleanArchitecture.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
